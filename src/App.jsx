c725918577029821c85f94630d9f5ed6618632ec
Keulyingrid <keulybsous@gmail.com>
2022-05-06 07:41:59 -0300

    --

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {Login} from './pages/login/index'
 import {Cliente} from './pages/cliente'
 import { FormModalCliente } from './Components/FormModalCliente/index'
 import {LadingPage} from './pages/Lading Page'
-
 export const App = () => {
   
   return (
