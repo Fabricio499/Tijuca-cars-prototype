b4dbeb524f12ae6ef5b6e8fd6c0ff984fdff7bea
dev-fabricio <devfabricio1@gmail.com>
2022-05-11 14:57:48 -0300

    11/05

--- src/Components/Form/FormCliente/index.jsx
+++ src/Components/Form/FormCliente/index.jsx
@@ -8,6 +8,10 @@ export const FormCliente = () => {
 
     const [cars, setCars] = useState([])
     const [carroAtual, setCarroAtual] = useState({})
+    const [dias, setDias] = useState(0)
+    const [dataRetirada, setDataRetirada] = useState('')
+    const [valorAluguel, setValorAluguel] = useState(0)
+    const [dataAtual, setDataAtual] = useState('')
 
     useEffect(()=>{
         async function dataCar(){
@@ -17,27 +21,57 @@ export const FormCliente = () => {
         dataCar();
     }, [])
 
-    const [data1, setData1] = useState('')
-    const [dias, setDias] = useState('')
     
-    useEffect(()=>{console.log(carroAtual.idCarro)}, [carroAtual])
+    useEffect(()=>{
+        setValorAluguel(carroAtual * dias)
+    }, [carroAtual, dias])
 
-    const [dataFinal, setDataFinal] = useState('')
 
+    function dataDaReserva() {
+        const PegarData = new Date();
+        const dia = String(PegarData.getDate()).padStart(2, '0');
+        const mes = String(PegarData.getMonth() + 1).padStart(2, '0');
+        const ano = PegarData.getFullYear();
+        setDataAtual(dia + '/' + mes + '/' + ano);
+    } 
+    
+
+    async function novoAluguel(
+        modelo,
+        dataReserva,
+        dataRetirada,
+        dataEntrega,
+        qtdeDiasAlugados,
+        status
+        ) {
+        if(valorAluguel > 0){
+            dataDaReserva();
+            console.log(dataAtual)
+           /* Api.post('alugueis/novoAluguel' {
+                idCarro: CarroAtual,
+                dataReserva: ,
+                dataRetirada: ,
+                dataEntrega: ,
+                qtdeDiasAlugados: ,
+                status: ,
+            }) */
+        } 
+    }
 
     return(
         <ContainerModalCliente>
             <div className='form-model-cliente'>
             <div className='campo-input'>   
                 <label>Modelo do Veículo</label>
-                <select>
-                    <option value="---" selected>selecione um modelo</option>
+                <select onChange={e => setCarroAtual(e.target.value)}>
+                    <option value="" selected>
+                        selecione um modelo
+                    </option>
                     {cars.length > 0 && 
                         cars.map((cars)=>(
                             <option 
-                                onChange={e=>setCarroAtual(e.target.value)}
                                 value={cars.idCarro}
-                            >{cars.modelo}</option>
+                            >{cars.modelo}  -  {cars.placa}</option>
                         ))
                     }
                 </select>
@@ -46,9 +80,9 @@ export const FormCliente = () => {
             <div className='campo-input'>
                 <label>Data Retirada</label>
                 <input 
-                    type="date" 
-                    value={data1} 
-                    onChange={e=>setData1(e.target.value)} 
+                    value={dataRetirada}
+                    type="date"  
+                    onChange={e=>setDataRetirada(e.target.value)} 
                 />
             </div>
             <div className='campo-input'> 
@@ -59,18 +93,18 @@ export const FormCliente = () => {
                     min="1"
                     max="7"
                     onChange={e=>setDias(e.target.value)}
+                    onKeyDown={(e) => e.preventDefault()}
                     />
             </div>
             <div className='price-div'>
                 <p>Preço:</p>
-                <span></span>
+                <span>R${valorAluguel > 0 ? valorAluguel : '0'}</span>
             </div>
             </div>
 
             <div className='submit-button-alugar'>
-            <ButtonSubmit text="Alugar" />
+            <ButtonSubmit text="Alugar" onClick={novoAluguel}/>
             </div>
         </ContainerModalCliente>
     )
 }
-
